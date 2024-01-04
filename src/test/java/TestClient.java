4039fa78539234b04aeab32f276aeb76b5bea873
Bruno Rodrigo <bruno.rodrigo@definedcrowd.com>
2024-01-04 14:18:49 +0000

    Add initial files

---
+++ src/test/java/TestClient.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class TestClient {
+
+    @Test
+    @DisplayName("When get all clients without register clients, Then the list should be empty")
+    public void getAllClients(){
+
+    }
+}
